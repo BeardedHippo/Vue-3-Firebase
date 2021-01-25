78255b642fd5b2ce6277d31936012b05cc6cd400
Patrickdegreat <patrick.de.great@gmail.com>
2021-01-25 15:02:15 +0100

    asd
    asd

---
+++ app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: '45',
+            showBooks: true,
+        }
+    },
+    methods: {
+        changeTitle(title) {
+        //    this.title = 'Words of Randiance';
+        this.title = title;
+        },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks;
+        }
+    }
+});
+app.mount('#app');
